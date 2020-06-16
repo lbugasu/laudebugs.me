eedfdfe96abf69e718aead895e9fb10d89d8ca77
lbugasu <lbugasu@gmail.com>
2020-06-16 12:04:14 -0400

    Updates

---
+++ precache-manifest.4097eaeeffb3cd18479b8cdaeb87ac75.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78bc1bc6c54dcd5e0b95140582b2280",
+    "url": "/laudebugs.me/index.html"
+  },
+  {
+    "revision": "e922ea167f343e12bf3d",
+    "url": "/laudebugs.me/static/css/main.aedf2736.chunk.css"
+  },
+  {
+    "revision": "d858f85c81240078bee9",
+    "url": "/laudebugs.me/static/js/2.ee65749e.chunk.js"
+  },
+  {
+    "revision": "12bcb611199202335d77fd6a73a37f8e",
+    "url": "/laudebugs.me/static/js/2.ee65749e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e922ea167f343e12bf3d",
+    "url": "/laudebugs.me/static/js/main.ad4feb77.chunk.js"
+  },
+  {
+    "revision": "b06afe2f90dadc0f2443",
+    "url": "/laudebugs.me/static/js/runtime-main.a4cbf8f6.js"
+  },
+  {
+    "revision": "f801e234d51049bd538d3d95f6f4e110",
+    "url": "/laudebugs.me/static/media/GlacialIndifference-Bold.f801e234.otf"
+  },
+  {
+    "revision": "ada009574362137f92450ef2c828946d",
+    "url": "/laudebugs.me/static/media/Posts.ada00957.less"
+  },
+  {
+    "revision": "1157d6bc1c4f9bb8074290ae7f1c35db",
+    "url": "/laudebugs.me/static/media/SinglePost.1157d6bc.less"
+  },
+  {
+    "revision": "c7b8b2f4d13e03c1527e2a09afd9395f",
+    "url": "/laudebugs.me/static/media/typewriter-large.c7b8b2f4.jpg"
+  }
+]);
--- src/Pages/NotFoundPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const NotFoundPage = () => (
-  <>
-    <h1>Work</h1>
-    <h1>Notes</h1>
-  </>
-);
-
-export default NotFoundPage;
