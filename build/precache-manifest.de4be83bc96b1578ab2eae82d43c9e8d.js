f400f609a7c00101b5f82e7858e319121b6f5d36
lbugasu <lbugasu@gmail.com>
2020-06-17 13:48:29 -0400

    heroku deploy

---
+++ build/precache-manifest.de4be83bc96b1578ab2eae82d43c9e8d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb72f48b790876de755ed732048bb3be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdedd88c5eb0b8afc2b0",
+    "url": "/static/css/main.4c5ba112.chunk.css"
+  },
+  {
+    "revision": "d858f85c81240078bee9",
+    "url": "/static/js/2.ee65749e.chunk.js"
+  },
+  {
+    "revision": "12bcb611199202335d77fd6a73a37f8e",
+    "url": "/static/js/2.ee65749e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdedd88c5eb0b8afc2b0",
+    "url": "/static/js/main.967cc3c6.chunk.js"
+  },
+  {
+    "revision": "8ca2dc468f5d978663ce",
+    "url": "/static/js/runtime-main.f2a20adc.js"
+  },
+  {
+    "revision": "f801e234d51049bd538d3d95f6f4e110",
+    "url": "/static/media/GlacialIndifference-Bold.f801e234.otf"
+  },
+  {
+    "revision": "a696beac4ba45cbb5cd92b4fa8e7d83c",
+    "url": "/static/media/Posts.a696beac.less"
+  },
+  {
+    "revision": "aceae8da47103202dcfe68f8a3c01436",
+    "url": "/static/media/SinglePost.aceae8da.less"
+  },
+  {
+    "revision": "c7b8b2f4d13e03c1527e2a09afd9395f",
+    "url": "/static/media/typewriter-large.c7b8b2f4.jpg"
+  }
+]);
