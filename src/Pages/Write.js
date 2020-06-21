4c75c7a52b77d11ae3be671a2b1b1cb232e2afc0
lbugasu <lbugasu@gmail.com>
2020-06-21 13:12:59 -0400

    added thank you note for message sent

--- src/Pages/Write.js
+++ src/Pages/Write.js
@@ -1,46 +1,88 @@
-import React from "react";
-import NavBarHome from "../components/NavBarHome";
-import FrontImage from "../components/FrontImage";
+import React,{useState, useEffect} from "react";
+// import NavBarHome from "../components/NavBarHome";
+// import FrontImage from "../components/FrontImage";
 import GeneralHeader from "../components/GeneralHeader";
-const Homepage = ({ match }) => (
-  <>
-    <GeneralHeader />
-    <h1 className="write">Write to me</h1>
-    <h2 className="write">
-      <em>I write back</em> (◍•ᴗ•◍)
-    </h2>
+import { Redirect } from "react-router-dom";
 
-    <div className="WriteToMe">
-      <form>
-        <label>
-          <br></br>
-          <textarea
-            id="thoughts"
-            name="message"
-            rows="7"
-            cols="50"
-            placeholder="Have a question or something to share... Or maybe something you'd like to work on together... Type on"
-          ></textarea>
-        </label>
-        <div className="sending">
-          <input id="thoughts" name="email" placeholder="your email" required />
 
-          <br></br>
-          <div id="send" className="send">
-            <button
-              id="sendButton"
-              data-sitekey="6LcR_aYZAAAAAGbW-vwJK2GG5A06YiLNHR0Ka1pP"
-              class="g-recaptcha"
-              data-callback="onSubmit"
-              data-action="submit"
-            >
-              - Send away -
-            </button>
-          </div>
+export default function Homepage({ match }){
+  const [message, setMessage] = useState('')
+  const [email, setEmail] = useState('')
+
+  const sendMessage = async ()=>{
+    const response = await fetch('http://localhost:8000/api/message',{
+      method:'post',
+      mode : 'no-cors',
+      credentials: 'same-origin',
+      body:`email=${email}&message=${message}`,
+      headers: {
+        // 'Content-Type': 'application/json'
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+    const frm = document.getElementById("frm")
+    const parent = frm.parentElement
+    parent.removeChild(frm)
+    const received = document.createElement('div')
+    received.innerHTML = `
+    <h3>Thank you - I will be in tough soon(^^)<h3/>
+    `;
+    parent.append(received)
+    // const body = await result.json()
+    // console.log(body)
+    // return response.json()
+  }
+  const renderPage = ()=>{
+    return (
+      <>
+        <h1 className="write">Write to me</h1>
+        <h2 className="write">
+          <em>I write back</em> (◍•ᴗ•◍)
+        </h2>
+        <div className="WriteToMe">
+          <form id="frm" action="/api/contact" method="post">
+            <label>
+              <br></br>
+              <textarea
+                id="thoughts"
+                name="message"
+                rows="7"
+                cols="50"
+                placeholder="Have a question or something to share... Or maybe something you'd like to work on together... Type on"
+                value={message} onChange = {(event)=>setMessage(event.target.value)}
+              ></textarea>
+            </label>
+            <div className="sending">
+              <input id="thoughts" name="email" placeholder="your email" required value={email} onChange={(event)=>setEmail(event.target.value)}/>
+    
+              <br></br>
+              <div id="send" className="send">
+                <button
+                  id="sendButton"
+                  data-sitekey="6LcR_aYZAAAAAGbW-vwJK2GG5A06YiLNHR0Ka1pP"
+                  class="g-recaptcha"
+                  data-callback="onSubmit"
+                  data-action="submit"
+                  onClick={()=>{
+                    sendMessage()
+                    
+                    }}
+                >
+                  - Send away -
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
-  </>
-);
+      </>
+    );
+  }
+  return (
+    <>
+      <GeneralHeader />
+      {renderPage()}
+    </>
+  )
+}
 
-export default Homepage;
+
