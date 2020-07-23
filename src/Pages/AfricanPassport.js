534a01fe555d4da86ab4c3f4e09623d8f089cf0e
lbugasu <lbugasu@gmail.com>
2020-07-23 19:59:16 -0400

    Thinking changes

--- src/Pages/AfricanPassport.js
+++ src/Pages/AfricanPassport.js
@@ -3,7 +3,11 @@ import React from "react";
 import AfricanPassportHeader from "../components/AfricanPassportHeader";
 
 import "./css/AfricanPassport.css";
-
+import apple from "../css/images/apple.png";
+import spotify from "../css/images/spotify.png";
+import youtube from "../css/images/youtube.png";
+import show from "../css/images/show.png"
+import close from "../css/images/close.png"
 const printMusic = () => {
   const music = require("./music.json");
 
@@ -15,6 +19,27 @@ const printMusic = () => {
           <h1 className="albumTitle">
             {note.artist} - {note.title}
           </h1>
+          <div className="button">
+          <img className="button" src={show} alt={note.title}></img>
+          </div>
+
+        </div>
+        <div className="listen">
+          <a href={note.appleMusic}
+            target="__blank"
+          >
+            <img src={apple} alt="listen on apple music"></img>
+          </a>
+          <a href={note.spotify}
+            target="__blank"
+          >
+            <img src={spotify} alt="Listen on Spotify"></img>
+          </a>
+            <a href={note.youtubeMusic}
+            target="__blank"
+            >
+          <img src={youtube} alt="listen on Youtube music"></img>
+          </a>
         </div>
       </div>
     </>
