30d25fbc08cf9b9dd56b58c7dce3977543db0a38
lbugasu <lbugasu@gmail.com>
2020-07-23 17:19:24 -0400

    🏄‍♂️ Fixed background in ap page

---
+++ build/precache-manifest.1a9d84dd7bf1be8be6811dc98750a681.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556fd34972b2acf7c3c38dd4ccaf94a1",
+    "url": "/laudebugs.me/index.html"
+  },
+  {
+    "revision": "cdbde5d58c64b2920240",
+    "url": "/laudebugs.me/static/css/main.78d34302.chunk.css"
+  },
+  {
+    "revision": "4e1c008644cc93b3deaa",
+    "url": "/laudebugs.me/static/js/2.188db092.chunk.js"
+  },
+  {
+    "revision": "12bcb611199202335d77fd6a73a37f8e",
+    "url": "/laudebugs.me/static/js/2.188db092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdbde5d58c64b2920240",
+    "url": "/laudebugs.me/static/js/main.1727ebdb.chunk.js"
+  },
+  {
+    "revision": "81e49ea87e8d5b51dbfd",
+    "url": "/laudebugs.me/static/js/runtime-main.23d15698.js"
+  },
+  {
+    "revision": "9818302bde1c74eaa2f0331c0b0b1189",
+    "url": "/laudebugs.me/static/media/Cardo-Regular.9818302b.ttf"
+  },
+  {
+    "revision": "f801e234d51049bd538d3d95f6f4e110",
+    "url": "/laudebugs.me/static/media/GlacialIndifference-Bold.f801e234.otf"
+  },
+  {
+    "revision": "3468484bab4aaad9c9d98948685f4f70",
+    "url": "/laudebugs.me/static/media/HighTide-Regular.3468484b.ttf"
+  },
+  {
+    "revision": "a696beac4ba45cbb5cd92b4fa8e7d83c",
+    "url": "/laudebugs.me/static/media/Posts.a696beac.less"
+  },
+  {
+    "revision": "aceae8da47103202dcfe68f8a3c01436",
+    "url": "/laudebugs.me/static/media/SinglePost.aceae8da.less"
+  },
+  {
+    "revision": "2d0c3cc038a4d105fe2fdfacf4810362",
+    "url": "/laudebugs.me/static/media/experiments.2d0c3cc0.png"
+  },
+  {
+    "revision": "2b97990d9d851894358cc9e3676e790f",
+    "url": "/laudebugs.me/static/media/eyeem.2b97990d.png"
+  },
+  {
+    "revision": "9cc7e81ec42847a19caa0147ce8c0c0f",
+    "url": "/laudebugs.me/static/media/github.9cc7e81e.png"
+  },
+  {
+    "revision": "58bc5b2a945407063815a0801ff2bb5a",
+    "url": "/laudebugs.me/static/media/li.58bc5b2a.png"
+  },
+  {
+    "revision": "86e332710eae1361b9aa8725cf777c7c",
+    "url": "/laudebugs.me/static/media/resume.86e33271.webp"
+  },
+  {
+    "revision": "c7b8b2f4d13e03c1527e2a09afd9395f",
+    "url": "/laudebugs.me/static/media/typewriter-large.c7b8b2f4.jpg"
+  }
+]);
